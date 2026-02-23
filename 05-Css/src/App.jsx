113d4ad69645dadbccf58e934b24e8dd8c011883
sharib54009 <mohammedsharib0704@gmail.com>
2026-02-23 20:03:13 +0530

    practiced tailwind css

---
+++ 05-Css/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-500 text-white m-4'>
+      <h1>Mohammed sharib</h1>
+    </div>
+  )
+}
+
+export default App
