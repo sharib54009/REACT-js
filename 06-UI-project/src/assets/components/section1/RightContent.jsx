1da10be4bcf0c8298120ec71c526ece4bb596ec1
sharib54009 <mohammedsharib0704@gmail.com>
2026-03-25 17:33:45 +0530

    created a UI project

--- 06-UI-project/src/assets/components/section1/RightContent.jsx
+++ 06-UI-project/src/assets/components/section1/RightContent.jsx
@@ -4,7 +4,7 @@ import RightCard from './RightCard'
 const RightContent = ({ users }) => {
   console.log(users)
   return (
-    <div id ='right' className='h-full w-2/3 overflow-x-auto p-7 rounded-4xl flex justify-between gap-10  bg-gray-100 bg-neutral-100 bg-zinc-100'>
+    <div id =' bg-gray-100 bg-neutral-100 bg-zinc-100 right' className='h-full w-2/3 overflow-x-auto p-7 rounded-4xl flex justify-between gap-10  '>
       {users.map((elem, idx) => (
         <RightCard
           key={elem.id || idx}
